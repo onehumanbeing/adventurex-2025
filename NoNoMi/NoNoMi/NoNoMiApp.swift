ade23f439badb83722a82f2cc991ad47bbf6cf5d
Henry <henry@HenrydeMacBook-Pro.local>
2025-07-24 17:40:23 +0800

    first commit QAQ

---
+++ NoNoMi/NoNoMi/NoNoMiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoNoMiApp.swift
+//  NoNoMi
+//
+//  Created by Henry on 24/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoNoMiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoNoMi/NoNoMiTests/NoNoMiTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoNoMiTests.swift
+//  NoNoMiTests
+//
+//  Created by Henry on 24/7/2025.
+//
+
+import Testing
+@testable import NoNoMi
+
+struct NoNoMiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
